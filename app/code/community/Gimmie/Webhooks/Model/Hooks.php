edca6341a220e76bb33ae952976ae01c5249de8b
Maythee Anegboonlap <null@llun.in.th>
2014-09-05 11:27:42 +0800

    Add events and scripts head. #3, #4

--- app/code/community/Gimmie/Webhooks/Model/Hooks.php
+++ app/code/community/Gimmie/Webhooks/Model/Hooks.php
@@ -2,9 +2,27 @@
 class Gimmie_Webhooks_Model_Hooks {
 
   public function injectScripts(Varien_Event_Observer $observer = null) {
+    $layout = $observer->getEvent()->getLayout();
+    $block = $layout->createBlock(
+      "Mage_Core_Block_Template",
+      "gimmie_scripts",
+      array("template" => "gimmie/scripts.phtml")
+    );
+
+    $head = $layout->getBlock("head");
+    $head->append($block);
   }
 
   public function dispatchRegisterSuccess(Varien_Event_Observer $observer = null) {
+  }
+
+  public function dispatchLoginSuccess(Varien_Event_Observer $observer = null) {
+  }
+
+  public function dispatchViewItem(Varien_Event_Observer $observer = null) {
+  }
+
+  public function dispatchPurchaseItem(Varien_Event_Observer $observer = null) {
   }
 
 }
