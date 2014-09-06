8521407692d388ab2517936c640c595be585d7bb
Maythee Anegboonlap <null@llun.in.th>
2014-09-06 21:39:38 +0800

    Add generate package.xml script.

--- gulpfile.js
+++ gulpfile.js
@@ -3,45 +3,87 @@ var gulp = require('gulp')
   , crypto = require('crypto')
   , fs = require('fs')
   , path = require('path')
+  , moment = require('moment')
   , util = require('util')
-  , xml = require('js2xmlparser')
+  , xml = require('xmlbuilder')
   , _ = require('lodash')
+
+var multiline = function (fn) {
+  return fn.toString().replace(/(^function\s*\(\)\s+{\s+\/\*)|(\s*\*\/\;*\s*})/g, '')
+}
 
 gulp.task('build', function () {
 
-  var block = function (name) { return { '@': { name: name } } }
-  var contents = function (dir, tree) {
+  var buildTree = function (dir, tree) {
     var base = fs.readdirSync(dir)
 
     base.forEach(function (file) {
       var full = path.join(dir, file)
-      var child = block(file)
+      if (!tree['#list']) { tree['#list'] = [] }
 
       if (fs.statSync(full).isDirectory()) {
-        if (!tree['dir']) { tree['dir'] = [] }
-
-        tree['dir'].push(child)
-        contents(full, child)
+        var child = { 'dir': { '@name': file } }
+        tree['#list'].push(child)
+        buildTree(full, child['dir'])
       }
       else {
-        if (!tree['file']) { tree['file'] = [] }
-
         var md5 = crypto.createHash('md5')
         var content = fs.readFileSync(full, { encoding: 'utf8' })
         md5.update(content)
-       
-        child['@'].hash = md5.digest('hex')
-        tree['file'].push(child)
+      
+        var child = { 'file': { '@name': file, '@hash': md5.digest('hex') } }
+        tree['#list'].push(child)
       }
     })
 
     return tree
   }
 
-  var community = contents(path.join(__dirname, 'app', 'code', 'community'), block('magecommunity'))
-  var design = contents(path.join(__dirname, 'app', 'design'), block('magedesign'))
-  var etc = contents(path.join(__dirname, 'app', 'etc'), block('mageetc'))
-  console.log (xml('content', { target: [ community, design, etc ] }))
+  var meta = {
+    'name': 'webhooks',
+    'version': '1.0.0',
+    'stability': 'stable',
+    'license': 'MITL',
+    'channel': 'community',
+    'extends': {},
+    'summary': 'Summary',
+    'description': {
+      '#cdata': multiline(function() {
+/*
+Multiline content here
+ */
+      })
+    },
+    'notes': {
+      '#cdata': multiline(function() {
+/*
+Other multiline here
+ */
+      })
+    },
+    'authors': [
+      { 'author': {
+          'name': 'Maythee Anegboonlap',
+          'user': 'maythee',
+          'email': 'maythee@gimmie.io'
+        } 
+      }
+    ],
+    'date': moment().format('YYYY-MM-DD'),
+    'time': moment().format('hh:mm:ss'),
+    'contents': [
+      { target: buildTree(path.join(__dirname, 'app', 'code', 'community'), { '@name': 'magecommunity' }) },
+      { target: buildTree(path.join(__dirname, 'app', 'design'), { '@name': 'magedesign' }) },
+      { target: buildTree(path.join(__dirname, 'app', 'etc'), { '@name': 'mageetc' }) }
+    ],
+    'compatible': {},
+    'dependencies': {
+      'required': {
+        'php': { 'min': '5.3.0', 'max': '6.0.0' }
+      }
+    }
+  }
+  fs.writeFileSync('package.xml', xml.create('package').ele(meta).end({ pretty: true }))
 
 })
 
