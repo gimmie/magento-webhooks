902b3ec7d7d1179ba3be27e2f34a2844564a6804
Maythee Anegboonlap <null@llun.in.th>
2014-09-09 17:37:02 +0800

    Add more forms for add application #2.

--- app/code/community/Gimmie/Webhooks/controllers/Adminhtml/WebhooksController.php
+++ app/code/community/Gimmie/Webhooks/controllers/Adminhtml/WebhooksController.php
@@ -15,6 +15,26 @@ class Gimmie_Webhooks_Adminhtml_WebhooksController extends Mage_Adminhtml_Contro
     $this->renderLayout();
   }
 
+  public function redirectToAppAction() {
+    $this->loadLayout();
+    $this->_setActiveMenu('gimmie/webhooks');
+    $this->renderLayout();
+  }
+
+  public function oauthAction() {
+    $this->loadLayout();
+    $this->_setActiveMenu('gimmie/webhooks');
+
+    $block = $this->getLayout()->createBlock(
+      "Mage_Core_Block_Template",
+      "webhooks_admin",
+      array("template" => "gimmie/allow.phtml")
+    );
+    $this->_addContent($block);
+
+    $this->renderLayout();
+  }
+
   protected function _isAllowed() {
     return Mage::getSingleton('admin/session')->isAllowed('gimmie');
   }
--- app/code/community/Gimmie/Webhooks/controllers/OAuthController.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Gimmie_Webhooks_OAuthController extends Mage_Core_Controller_Front_Action {
-}
-?>
