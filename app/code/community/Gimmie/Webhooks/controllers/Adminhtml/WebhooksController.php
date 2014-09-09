dcf0df3ff6c19b05ff75fd2b5bd388d8d0c08107
Maythee Anegboonlap <null@llun.in.th>
2014-09-09 12:54:16 +0800

    Add template for adding app page. #2

--- app/code/community/Gimmie/Webhooks/controllers/Adminhtml/WebhooksController.php
+++ app/code/community/Gimmie/Webhooks/controllers/Adminhtml/WebhooksController.php
@@ -4,6 +4,14 @@ class Gimmie_Webhooks_Adminhtml_WebhooksController extends Mage_Adminhtml_Contro
   public function indexAction() {
     $this->loadLayout();
     $this->_setActiveMenu('gimmie/webhooks');
+
+    $block = $this->getLayout()->createBlock(
+      "Mage_Core_Block_Template",
+      "webhooks_admin",
+      array("template" => "gimmie/apps.phtml")
+    );
+    $this->_addContent($block);
+
     $this->renderLayout();
   }
 
