c04b5453a20c29d5e66d5b9e3c81baf1e7bc46a0
Maythee Anegboonlap <null@llun.in.th>
2014-09-11 12:02:01 +0800

    Add fields and config for #6.

--- app/code/community/Gimmie/Webhooks/sql/webhooks_setup/mysql4-install-1.0.0.php
+++ app/code/community/Gimmie/Webhooks/sql/webhooks_setup/mysql4-install-1.0.0.php
@@ -4,12 +4,19 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-DROP TABLE IF EXISTS `{$this->getTable('webhooks')}`;
-CREATE TABLE `{$this->getTable('webhooks')}` (
-    `webhook_id` int(11) unsigned NOT NULL auto_increment,
-    `url`        text NOT NULL,
-    `config`     text NOT NULL,
-    PRIMARY KEY (`webhook_id`)
+DROP TABLE IF EXISTS `{$this->getTable('webhooks_application')}`;
+CREATE TABLE `{$this->getTable('webhooks_application')}` (
+  `application_id` int(11) unsigned NOT NULL auto_increment,
+  `domain` text NOT NULL,
+  `url` text NOT NULL,
+  `description` text NOT NULL,
+  `title` text NOT NULL,
+  `logo` text NOT NULL,
+  `key` text NOT NULL,
+  `scripts` text NOT NULL,
+  `events` text NOT NULL,
+  `enable` Bool NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`application_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
 
