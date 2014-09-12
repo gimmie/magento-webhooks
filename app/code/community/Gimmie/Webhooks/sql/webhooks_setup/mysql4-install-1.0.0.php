b4753a2f303c18a44b0b7c4af7c076cbbfc58081
root <root@boot2docker.(none)>
2014-09-12 17:56:38 +0000

    Add unique contraints on secret and domain

--- app/code/community/Gimmie/Webhooks/sql/webhooks_setup/mysql4-install-1.0.0.php
+++ app/code/community/Gimmie/Webhooks/sql/webhooks_setup/mysql4-install-1.0.0.php
@@ -7,11 +7,11 @@ $installer->run("
 DROP TABLE IF EXISTS `{$this->getTable('webhooks_application')}`;
 CREATE TABLE `{$this->getTable('webhooks_application')}` (
   `application_id` int(11) unsigned NOT NULL auto_increment,
-  `domain` text NOT NULL,
+  `domain` varchar(255) NOT NULL UNIQUE,
   `description` text NOT NULL,
   `title` text NOT NULL,
   `logo` text NOT NULL,
-  `secret` text NOT NULL,
+  `secret` varchar(64) NOT NULL UNIQUE,
   `scripts` text NOT NULL,
   `events` text NOT NULL,
   `enable` Bool NOT NULL DEFAULT FALSE,
